3d9a8bed4e8121bb91af3da215d9604b75f423b8
KenanV20 <kverdiyev34@gmail.com>
2024-04-14 03:49:18 +0400

    test55

--- src/main/java/com/example/exam/exam/service/auth/JwtService.java
+++ src/main/java/com/example/exam/exam/service/auth/JwtService.java
@@ -67,7 +67,7 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 2588))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 22))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
